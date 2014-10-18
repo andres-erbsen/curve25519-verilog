8c7e8f31fd7704fb8017ef5cdcf39b992e7c4a93
Andres Erbsen <andres@krutt.org>
2014-10-18 14:10:09 -0400

    GF(2^255-19) multiplication: final speculative reduction, first tests

--- femul.v
+++ femul.v
@@ -3,10 +3,11 @@
 module femul(input wire clock, start,
              input wire [254:0] a_in, b_in,
              output reg done = 0,
-             output reg [254:0] out = 0);
+             output wire [254:0] out);
     parameter W = 17; // hardware multiplier input word size
     parameter N = 15; // number of words used to represent one field element
-    parameter C = 19; // pseudo-Mersenne coefficient; P = (1<<W*N) - C = 2^255 - 19
+    parameter C = 19; // pseudo-Mersenne coefficient
+    parameter P = (255'b1<<(N*W)) - C; // 2^255 - 19
     parameter LOGC = 4;
     parameter LOGN = 4;
 
@@ -22,19 +23,19 @@ module femul(input wire clock, start,
             b <= b_in;
         end else if (multiply_step < N) begin
             multiply_step <= multiply_step + 1;
-            a <= {a, a[254 -: W]}; // XXX: rotation operators <<< and >>> DO NOT WORK?
+            a <= {a[0 +: N*W-W], a[254 -: W]}; // XXX: rotation operators <<< and >>> DO NOT WORK?
             b <= {b[0 +: W], b[W +: 255-W]};
             if (multiply_step == N-1) reduce_step <= 0;
         end
     end
 
-    genvar i; generate for (i = 0; i < N; i = i + 1) begin: mul_mid
+    genvar j; generate for (j = 0; j < N; j = j + 1) begin: mul_mid
         always @(posedge clock) begin
-            `define partial (b[0 +: W] * a[i*W +: W])
-            mid[i] <=                multiply_step == 0?          `partial     :
-                i < multiply_step && multiply_step < N ? mid[i] + `partial * C : 
-                                     multiply_step < N ? mid[i] + `partial     :
-                                      /* otherwise */    mid[i]                ;
+            `define partial (b[0 +: W] * a[j*W +: W])
+            mid[j] <=                multiply_step == 0?          `partial     :
+                j < multiply_step && multiply_step < N ? mid[j] + `partial * C : 
+                                     multiply_step < N ? mid[j] + `partial     :
+                                      /* otherwise */    mid[j]                ;
             `undef partial
         end
     end endgenerate
@@ -42,18 +43,34 @@ module femul(input wire clock, start,
     parameter R = 2; // number of high-order words where carry needs full reduction mod P
     parameter LOGR = 2;
     reg [LOGR+LOGN-1:0] reduce_step = N+R;
-    reg [2*W + LOGN + LOGC - 1:0] carry = 0;
-    wire reduce_running = reduce_step < R;
-    wire [W-1:0] carry_out = carry + mid[reduce_step - R];
+    reg [2*W + LOGN + LOGC - 1:0] carry = 0, carryP = 0;
+    reg [254:0] out_, outP; // two options for the output: r, and r-P
+    reg wrapP = 0;
+
+    wire [2*W + LOGN + LOGC - 1:0] partial, partialP, carry_next, carryP_next;
+    wire [LOGN-1:0] i = reduce_step == 0 ? N-2: 
+                        reduce_step == 1 ? N-1:
+                                           reduce_step-R;
+    assign partial  = carry  + mid[i];
+    assign partialP = carryP + mid[i] - P[i*W +: W];
+    assign carry_next  = reduce_step == 0 ?  mid[i]   >> W:
+                         reduce_step == 1 ? (partial  >> W) * C:
+                          /*otherwise*/      partial  >> W;
+    assign carryP_next = reduce_step == 0 ?  mid[i]   >> W:
+                         reduce_step == 1 ? (partial  >> W) * C:
+                          /*otherwise*/      partialP >> W;
     always @ (posedge clock) begin
         if (reduce_step < N+R) begin
             reduce_step <= reduce_step + 1;
-            if (reduce_step >= R) out <= {carry_out, out[254:W]};
-            carry <= reduce_step == 0 ? mid[N-2] >> W:
-                     reduce_step == 1 ? ((mid[N-1] + carry) >> W) * C:
-                      /*otherwise*/    (carry + mid[reduce_step-R]) >> W;
+            if (reduce_step >= R) out_ <= {partial [W-1:0],  out[254:W]};
+            if (reduce_step >= R) outP <= {partialP[W-1:0], outP[254:W]};
+            carry <= carry_next;
+            carryP <= carryP_next;
         end
-        if (reduce_step == N+R-1) done <= 1;
-        else done <= 0;
+        if (reduce_step == N+R-1) begin
+            done <= 1;
+            wrapP <= ~|carryP_next;
+        end else done <= 0;
     end
+    assign out = wrapP ? outP : out_;
 endmodule
--- femul_test.v
+++ femul_test.v
@@ -1,18 +1,54 @@
 `default_nettype none
+`define assert(condition) if(!((|{condition})===1)) begin $display("FAIL"); $finish(1); end
 
 module femul_test;
-    reg clock = 0, start = 0;
-    reg [254:0] a = 25728561913544074806655338655832537372072648408242416352266576543536686506277;
-    reg [254:0] b = 17566812258234732776846655780981983198843065998106887116944655754324067075440;
+    parameter N = 5;
+    reg [$bits(N):0] i = 0;
+    reg [254:0] as [N:0];
+    reg [254:0] bs [N:0];
+    reg [254:0] outs [N:0];
+
+    initial begin
+        as[0]   = (255'd1<<128);
+        bs[0]   = (255'd1<<128);
+        outs[0] = 255'h026;
+
+        as[1]   = (255'd1<<255) - 1;
+        bs[1]   = 1;
+        outs[1] = 255'h012;
+
+        as[2]   = (1<<128) - 1;
+        bs[2]   = (1<<128) + 1;
+        outs[2] = 255'h025;
+
+        as[3]   = (1<<250) - 1997;
+        bs[3]   = 254'hfffffffff - 17;
+        outs[3] = 255'h37ffffffffffffffffffffffffffffffffffffffffffffffffff833980008c57;
+
+        as[4]   = 255'h6483b328032df78f6abb1342dc54964127be97507e17c1b4cf481339f1fa20de;
+        bs[4]   = 255'hb47d26181c9f63bb1405345faca4ffd0fe748b6652fa7d2decf0e2c865e988d;
+        outs[4] = 255'h7587e6935be3c0628e7fa76da3931343283adb49a03f048998eb0f9b51a209ef;
+    end
+
     wire [254:0] out;
     wire done;
-    femul femul(clock, start, a, b, done, out);
-
+    reg clock = 0, start = 1;
+    femul femul(clock, start, as[i], bs[i], done, out);
     always #1 clock <= !clock;
-    initial begin
-        $monitor("done=%b mul_step=%x reduce_step=%d carry=%x mid[14]=%x out=%0x", done, femul.multiply_step, femul.reduce_step, femul.carry, femul.mid[14], out);
-        #1 start <= 1;
-        #1 start <= 0;
+
+    always @(posedge clock) begin
+        if (done) begin
+            $display("(0x%x * 0x%x)%%((1<<255)-19) == 0x%x", as[i], bs[i], out);
+            `assert(out === outs[i])
+            if (i < N-1) begin
+                i <= i + 1;
+                start <= 1;
+            end else begin
+                $finish;
+            end
+        end
+        if (start) start <= 0;
     end
-    always @(posedge done) $finish;
+
+    // initial begin $monitor("done=%b reduce_step=%d carry=%x partial=%x      out=%x", done, femul.reduce_step, femul.carry, femul.partial, out); end
 endmodule
