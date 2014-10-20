c00ec6870c0732900799e8812b352748a4faf5ed
Andres Erbsen <andres@krutt.org>
2014-10-20 16:30:33 -0400

    feexp: random test

--- feexp_test.v
+++ feexp_test.v
@@ -2,7 +2,7 @@
 `define assert(condition) if(!((|{condition})===1)) begin $display("FAIL"); $finish(1); end
 
 module feexp_test;
-    parameter N = 5;
+    parameter N = 6;
     reg [$bits(N):0] i = 0;
     reg [254:0] as [N:0];
     reg [254:0] bs [N:0];
@@ -28,6 +28,10 @@ module feexp_test;
         as[4]   = 7;
         bs[4] = (255'b1 << 255) - 21;
         outs[4] = 255'h249249249249249249249249249249249249249249249249249249249249248d;
+
+        as[5]   = 255'h1342d411f5508b2013297c21c1ec44e4c21cf6d8da8f3f0cf8e333d64614ad4d;
+        bs[5]   = 255'h42ffa5e363133dab46c552c3ace4ffdd3c01ff6a72f09791922b63997b871740;
+        outs[5] = 255'h21f3b3f38a7a8a6213ccf88adf7bf9c403185fe8624447d8c321512f10db69d7;
     end
 
     wire [254:0] out;
